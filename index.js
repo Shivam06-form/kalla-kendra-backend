45c468398f6456844fb98befd2e70cb54109b576
SazzadurRa <sazzadurrahman000@gmail.com>
2022-10-28 21:10:06 +0600

    set signup

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 
-// delete node moduls
+// not found node modules
 // app.use((req, res, next) => {
 //   res.status(404).json({
 //     message: "resource not found",
--- routs/users.route.js
+++ routs/users.route.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const cors = require("cors");
 require("dotenv").config();
 
-const { getUsers, saveResume, Signup } = require("../controllers/users.controller");
+const {
+  getUsers,
+  saveResume,
+  Signup,
+} = require("../controllers/users.controller");
 const router = express.Router();
-
 router.get("/", getUsers);
 
 router.post("/resume", saveResume);
 
 router.post("/Signup", Signup);
-
 module.exports = router;
